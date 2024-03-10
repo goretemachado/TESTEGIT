378906fe42a66b37d89a78d76bf90c4831435b2f
MariaGorete <126599402+goretemachado@users.noreply.github.com>
2024-03-10 21:39:01 +0000

    My first commit

---
+++ testegit/src/main/java/testegit/TesteGit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testegit;
+
+/**
+ *
+ * @author goret
+ */
+public class TesteGit {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("123");
+
+    }
+    
+}
