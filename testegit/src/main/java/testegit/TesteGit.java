d3f5f450692bcbbdb5bf4bbee1a6ecf50b39d5b6
MariaGorete <126599402+goretemachado@users.noreply.github.com>
2024-03-10 21:40:02 +0000

    Acrescentei ABC

--- testegit/src/main/java/testegit/TesteGit.java
+++ testegit/src/main/java/testegit/TesteGit.java
@@ -12,7 +12,7 @@ public class TesteGit {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("123");
-
+        System.out.println("ABC");
     }
     
 }
